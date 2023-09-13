75a36d188638b27efaac5b900bbb937eb4fdc056
guguuu <126880807+guguCEEP@users.noreply.github.com>
2023-09-13 09:05:26 -0300

    Add files via upload

---
+++ gugu-numero/type.number.js
@@ -0,0 +1,24 @@
+//tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+//ponto flutuante
+
+const numeroPontoFluente = 3.3;
+const pontoFluenteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFluente;
+
+console.log(novaOperacao)
+
+//NaN -> Not A Number (não é um número)
+
+const Alura = "Alura";
+console.log(alura * primeiroNumero)
